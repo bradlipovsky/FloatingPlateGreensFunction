aa186a6fed992a5db3fe139d635d1260feafc912
Brad Lipovsky <bradlipovsky@gmail.com>
2020-02-05 08:53:40 -0500

    consistent notation

--- SemiAnalyticGreenFunction.m
+++ SemiAnalyticGreenFunction.m
@@ -1,17 +1,14 @@
 function G = SemiAnalyticGreenFunction(model)
 
 Q = model.P/model.rhoi/model.h;
-C =  model.D*model.k.^4;
-B = model.rhoi*model.h; 
-A = sqrt(C./B); % Has units of frequency
+AlphaXi = ( model.D*model.k.^4 /  model.rhoi/model.h ).^(1/4); % Has units of frequency
 t = model.t;
 G = zeros(model.nx,model.nt);
 dx = model.L/model.nx;
 
 for i = 1:model.nt
-	Ghat = Q .* sin( t(i) * A) ./ A;
+	Ghat = Q .* sin( t(i) * AlphaXi.^2) ./ AlphaXi.^2;
 	Ghat(1) = Q * t(i); % Beam
 	G(:,i) = ifftshift(ifft(Ghat))/dx;
 end
 
-G = G * model.rhoi * model.h; % undo scaling; return physical units
